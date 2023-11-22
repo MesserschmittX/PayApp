7c256fa4ec227b69deac499b65f7bf2dc59c1133
Daniel Schröder <dani9818@gmx.de>
2023-11-22 23:30:44 +0100

    add flutter_paypal_native

---
+++ android/app/src/main/kotlin/com/example/payapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.payapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/paypal_service.dart
@@ -0,0 +1,104 @@
+import 'package:flutter_paypal_native/flutter_paypal_native.dart';
+import 'package:flutter_paypal_native/models/custom/currency_code.dart';
+import 'package:flutter_paypal_native/models/custom/environment.dart';
+import 'package:flutter_paypal_native/models/custom/order_callback.dart';
+import 'package:flutter_paypal_native/models/custom/purchase_unit.dart';
+import 'package:flutter_paypal_native/models/custom/user_action.dart';
+import 'package:flutter_paypal_native/str_helper.dart';
+
+class PaypalService {
+  final _flutterPaypalNativePlugin = FlutterPaypalNative.instance;
+  // log queue
+  static List<String> logQueue = [];
+
+  Function callback = () => {};
+
+  String getTransactions() {
+    String transactions = "";
+    for (var i = 0; i < logQueue.length; i++) {
+      transactions += logQueue[i] + "\n";
+    }
+    print("############");
+    print(transactions);
+    return transactions;
+  }
+
+  void makePayment(double price, callback) async {
+    await initPayPal();
+    this.callback = callback;
+
+    //add 1 item to cart. Max is 4!
+    if (_flutterPaypalNativePlugin.canAddMorePurchaseUnit) {
+      _flutterPaypalNativePlugin.addPurchaseUnit(
+        FPayPalPurchaseUnit(
+          // price
+          amount: price,
+
+          ///please use your own algorithm for referenceId. Maybe ProductID?
+          referenceId: FPayPalStrHelper.getRandomString(16),
+        ),
+      );
+    }
+
+    _flutterPaypalNativePlugin.makeOrder(
+      action: FPayPalUserAction.payNow,
+    );
+  }
+
+  Future<void> initPayPal() async {
+    //set debugMode for error logging
+    FlutterPaypalNative.isDebugMode = true;
+
+    //initiate payPal plugin
+    await _flutterPaypalNativePlugin.init(
+      //your app id !!! No Underscore!!! see readme.md for help
+      returnUrl: "com.example.payapp://paypalpay",
+      //client id from developer dashboard
+      clientID:
+          "AdMa4WtIendZju-VBwvie0cHQKSr5v7yYnIn10RGBVlTjuVDQocWKSdNOE2csieQ1Xg2QxP8DiM-w81B",
+      //sandbox, staging, live etc
+      payPalEnvironment: FPayPalEnvironment.sandbox,
+      //what currency do you plan to use?
+      currencyCode: FPayPalCurrencyCode.eur,
+      //action paynow?
+      action: FPayPalUserAction.payNow,
+    );
+
+    //call backs for payment
+    _flutterPaypalNativePlugin.setPayPalOrderCallback(
+      callback: FPayPalOrderCallback(
+        onCancel: () {
+          //user canceled the payment
+          logResult("cancel");
+        },
+        onSuccess: (data) {
+          //successfully paid
+          //remove all items from queue
+          _flutterPaypalNativePlugin.removeAllPurchaseItems();
+          String visitor = data.cart?.shippingAddress?.firstName ?? 'Visitor';
+          String address =
+              data.cart?.shippingAddress?.line1 ?? 'Unknown Address';
+          logResult(
+            "Order successful ${data.payerId ?? ""} - ${data.orderId ?? ""} - $visitor -$address",
+          );
+        },
+        onError: (data) {
+          //an error occured
+          logResult("error: ${data.reason}");
+        },
+        onShippingChange: (data) {
+          //the user updated the shipping address
+          logResult(
+            "shipping change: ${data.shippingChangeAddress?.adminArea1 ?? ""}",
+          );
+        },
+      ),
+    );
+  }
+
+  // all to log queue
+  logResult(String text) {
+    logQueue.add(text);
+    callback(text);
+  }
+}
